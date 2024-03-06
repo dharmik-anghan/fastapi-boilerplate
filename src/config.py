966553f5915ad846b232ef5997f4240318fd2f86
Dharmik Anghan <dharmikanghan09@gmail.com>
2024-03-06 10:59:34 +0530

    [Dharmik] Config added

--- src/config.py
+++ src/config.py
@@ -4,3 +4,5 @@ load_dotenv(os.getenv("ENV_FILE", ".env"))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
+    HOST = os.environ["HOST"]
+    PORT = os.environ["PORT"]
