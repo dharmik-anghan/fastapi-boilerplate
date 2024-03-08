87f21ba2df94aa10b5cbc99845faab05a9d01961
Dharmik Anghan <dharmikanghan09@gmail.com>
2024-03-08 15:02:35 +0530

    [Dharmik] alembic

--- src/remote/db/otp/model.py
+++ src/remote/db/otp/model.py
@@ -1,11 +1,14 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Boolean, Column, Integer, String, DateTime, ForeignKey
 from src.remote.db.database import Base
 from datetime import datetime
+
 
 class Otp(Base):
     __tablename__ = "otps"
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     otp = Column(String, nullable=False)
-    email = Column(String,  ForeignKey('users.email'), unique=True, nullable=False)
+    email = Column(String, ForeignKey("users.email"), unique=True, nullable=False)
+    is_verified = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=True)
